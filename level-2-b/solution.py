184c71c2ff12aee80674ac9775b9692b174dc27c
istiyakaminsanto <istiyakaminsanto@gmail.com>
2023-04-10 17:29:04 +0600

    level-2 b solution done

---
+++ level-2-b/solution.py
@@ -0,0 +1,25 @@
+def solution(xs):
+
+    if(xs.count(0) == len(xs)):
+        return(str(0))
+
+    if(len(xs) == 1 and len([n for n in xs if n < 0]) == 1):
+        return(str(xs[0]))
+
+    if(len([n for n in xs if n < 0]) == 1 and xs.count(0) == len(xs)-1):
+        return(str(0))
+
+    Val = 1
+
+    for i in xs:
+        if (i != 0 and i <= 1000):
+            Val *= i
+
+    if Val < 0:
+        BigNeg = max([n for n in xs if n < 0])
+        Val = Val/BigNeg
+
+    return(str(int(Val)))
+
+a = solution([-2, -3, 4, 5])
+print(a)
